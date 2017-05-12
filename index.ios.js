0d52139b1d532cb26437273aad3695a4d3a3a60a
Danish Mughal <mughal.danish@gmail.com>
2017-05-12 03:04:16 -0700

    Added header component

--- index.ios.js
+++ index.ios.js
@@ -2,11 +2,13 @@
 
 // Import a library to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { Text, AppRegistry } from 'react-native'; // Deconstructed Import
+import Header from './src/components/header';
+
 
 // Create a component
 const App = () => (
-    <Text>Hello World</Text>
+    <Header />   // Self-closing tag
 );
 
 // Render it to the device
