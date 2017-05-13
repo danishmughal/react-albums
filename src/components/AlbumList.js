3e21daefa37e0da05d3c060ac0ef0945fa067bbb
Danish Mughal <mughal.danish@gmail.com>
2017-05-13 16:37:08 -0700

    Basic axios get request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 // Class-based Component
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
-    console.log('Component will mount in AlbumList');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums/')
+      .then(response => this.setState({ albums: response.data }));
   }
+
   render() {
+    console.log(this.state);
+    
     return (
       <View>
         <Text>Album List!</Text>
