3d7bae6a05f9a982b4165642b859a18170b9fefc
Danish Mughal <mughal.danish@gmail.com>
2017-05-13 15:47:21 -0700

    Header styling + props for reusable header

--- index.ios.js
+++ index.ios.js
@@ -2,13 +2,13 @@
 
 // Import a library to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native'; // Deconstructed Import
+import { AppRegistry } from 'react-native'; // Deconstructed Import
 import Header from './src/components/header';
 
 
 // Create a component
 const App = () => (
-    <Header />   // Self-closing tag
+  <Header headerText={'Albums'} />   // Self-closing tag
 );
 
 // Render it to the device
